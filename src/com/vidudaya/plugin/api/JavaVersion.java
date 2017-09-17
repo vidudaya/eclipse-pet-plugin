00d9a75531d546bfa870c09c98f93fddf869b375
vidudaya <vnbandara@gmail.com>
2017-09-18 03:00:50 +0530

    added gson to parse string to JSON

--- src/com/vidudaya/plugin/api/JavaVersion.java
+++ src/com/vidudaya/plugin/api/JavaVersion.java
@@ -1,7 +1,14 @@
 package com.vidudaya.plugin.api;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.BrowserFunction;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class JavaVersion extends BrowserFunction {
 
@@ -14,10 +21,14 @@ public class JavaVersion extends BrowserFunction {
 	 * function(Object[]) method. This method will be invoked whenever
 	 * javascript running in the Browser makes a call with the function's name.
 	 */
-	
-	// Good resource of BrowserFunction : http://ramkulkarni.com/blog/using-java-objects-in-javascript-in-eclipse-swt-browser-control/
-	// Good resource of BrowserFunction : http://blog.vogella.com/2009/12/21/javascript-swt/
-	
+
+	// Good resource of BrowserFunction :
+	// http://ramkulkarni.com/blog/using-java-objects-in-javascript-in-eclipse-swt-browser-control/
+	// Good resource of BrowserFunction :
+	// http://blog.vogella.com/2009/12/21/javascript-swt/
+	// When adding a new JAR :
+	// https://stackoverflow.com/questions/5744520/adding-jars-to-a-eclipse-plugin
+
 	Browser browser = null;
 	String functionName = null;
 
@@ -36,6 +47,38 @@ public class JavaVersion extends BrowserFunction {
 		String javaVersion = System.getProperty("java.version");
 		String response = "Successfully called a Java function : Java Version = "
 				+ javaVersion;
-		return response;
+
+		String output = getsMap();
+
+		return output;
+	}
+
+	public String gets() {
+		String[] sentences = new String[3];
+		sentences[0] = "Hi";
+		sentences[1] = "Hello";
+		sentences[2] = "How";
+
+		Gson gson = new GsonBuilder().create();
+		String jsonArray = gson.toJson(sentences);
+
+		System.out.println("array in  : " + jsonArray);
+
+		return jsonArray;
+	}
+
+	public String getsMap() {
+
+		Map<String, Integer> map = new HashMap<>();
+		map.put("key1", 100);
+		map.put("key2", 300);
+		map.put("key3", 400);
+
+		Gson gson = new GsonBuilder().create();
+		String jsonArray = gson.toJson(map);
+
+		System.out.println("array in  : " + jsonArray);
+
+		return jsonArray;
 	}
 }
